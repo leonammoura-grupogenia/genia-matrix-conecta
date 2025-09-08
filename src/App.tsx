c6d59134ce080ae2ac5f6425bb0792e8a0f69aa4
leonammoura-grupogenia <leonam.moura@grupogenia.tec.br>
2025-09-08 09:11:54 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#1d1e1d' }}>
+    <span style={{ color: '#fff', fontSize: '24px', fontWeight: 'bold' }}>GenIA Matrix Conecta</span>
+  </div>
+}
+
+export default App
